3257308a0adf0c91b52ac8353da4ccf16dd6cf15
Josh Wood <wood2jp2@gmail.com>
2018-01-07 21:56:11 -0500

    react auth template

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  ReactiveBase,
+  CategorySearch,
+  RatingsFilter,
+  ResultCard
+} from '@appbaseio/reactivesearch';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi</h1>
+      </div>
+    )
+  }
+ }
+
+ export default App;
